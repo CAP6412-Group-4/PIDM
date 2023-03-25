cfb2d6f3e5d0d46caecbca8b3d0426feca7e136e
stefanwerleman <stefanwerleman@knights.ucf.edu>
2023-03-25 13:15:33 -0400

    Save pose

--- src/analyze_channel.py
+++ src/analyze_channel.py
@@ -12,6 +12,7 @@ os.environ['OPENBLAS_NUM_THREADS'] = '1'
 # NOTE: This is important for importing packages.
 sys.path.append(str(Path().parent))
 
+import torch
 import numpy as np
 import torchvision.transforms as transforms
 from PIL import Image
@@ -22,7 +23,7 @@ from src.log.log import initialize_logger
 logger = logging.getLogger(__name__)
 
 ANALYSIS_LOG = paths.BASE_DIR / "logs" / "analysis.log"
-
+POINTS = paths.BASE_DIR
 
 def load_npy(npy_path):
     return np.load(str(npy_path))
@@ -38,27 +39,40 @@ def output_channels(pose_npy) -> None:
                 print(f"{col} ", end="")
             print()
 
-def output_joint(pose, pose_npy):
+def output_joint(dest, pose_npy):
+    # (20, 256, 256)
     tensor = transforms.ToTensor()(pose_npy).cuda()
 
     for idx, point in enumerate(tensor):
+        # point = 1 - point
         rgb_pose = (255 * point).cpu().detach().numpy()
         img = Image.fromarray(rgb_pose)
         
         if img.mode != "RGB":
             img = img.convert("RGB")
 
-        img.save(f"point_{idx}.png")
+        dest.mkdir(exist_ok=True)
+        img.save(str(dest / f"point_{idx}.png"))
+
+def save_pose(dest, pose_npy):
+    tensor = transforms.ToTensor()(pose_npy).cuda()
+    
+    pose = torch.cat([1 - tensor[:3]], -2)
+    pose_arr = (255*pose.unsqueeze(0).permute(0,2,3,1).detach().cpu().numpy()).astype(np.uint8)[0]
+    Image.fromarray(pose_arr).save(str(dest / "pose.png"))
 
 def main(pose: int) -> int:
     
     try:
         reference_pose = f"reference_pose_{pose}.npy"
+        reference_pose_dir = paths.BASE_DIR / f"reference_pose_{pose}"
 
         logger.info("Outputting Reference Pose: %s", pose)
 
         pose_npy = load_npy(npy_path=paths.TARGET_POSE / reference_pose)
-        output_joint(pose, pose_npy)
+
+        save_pose(reference_pose, pose_npy)
+        output_joint(reference_pose_dir, pose_npy, pose)
 
     except Exception as ex:
         logger.exception(ex)
