2e4267d2209d36644dffc45e6f7ed4e337f24b3b
stefanwerleman <stefanwerleman@knights.ucf.edu>
2023-03-25 14:36:59 -0400

    More arguments

--- src/pidm.py
+++ src/pidm.py
@@ -46,7 +46,7 @@ def download_dataset() -> None:
         ZIP_FILE.unlink()
 
 
-def main(image_path: str, debug: bool = False) -> int:
+def main(image_path: str, algorithm: str, steps: int, num_poses: int, debug: bool = False) -> int:
     """
     Main script for running the pose generator
 
@@ -68,7 +68,9 @@ def main(image_path: str, debug: bool = False) -> int:
         
         predictor = Predictor()
         
-        predictor.predict_pose(image=image_path, sample_algorithm="ddim", num_poses=1, nsteps=50)
+        predictor.predict_pose(
+            image=image_path, sample_algorithm=algorithm, num_poses=num_poses, nsteps=steps
+        )
     except Exception as ex:
         exit_code = 1
         logger.exception(ex)
@@ -83,10 +85,23 @@ if __name__ == "__main__":
     
     parser.add_argument("-i", "--image-path", required=False, default=str(PERSON_JPEG_PATH),
                         help="Path to image to use for pose generation.")
+    parser.add_argument("-a", "--algorithm", required=False, default="ddim",
+                        help="Can use either 'ddim' or 'ddpm'")
+    parser.add_argument("-s", "--steps", required=False, default=50,
+                        help="Number of steps")
+    parser.add_argument("-p", "--num-poses", required=False, default=4, 
+                        help="Number of random poses to generate")
+
     parser.add_argument("-d", "--debug", required=False, default=False, action="store_true",
                         help="Flag for also displaying DEBUG logs. "
                         "This would output more detailed logs.")
     
     args, _ = parser.parse_known_args()
     
-    sys.exit(main(image_path=args.image_path, debug=args.debug))
+    sys.exit(main(
+        image_path=args.image_path, 
+        algorithm=args.algorithm,
+        steps=args.steps,
+        num_poses=args.num_poses,
+        debug=args.debug
+    ))
